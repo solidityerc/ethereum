2fb95118a4d7eefe301d5732afe00a1c76f6cfbf
solidityerc <114520694+solidityerc@users.noreply.github.com>
2022-09-27 20:45:50 +0200

    Update IUniswapV2Pair.sol

--- IUniswapV2Pair.sol
+++ IUniswapV2Pair.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 /**
@@ -7,6 +7,6 @@ pragma solidity ^0.8.0;
  */
 
 contract solidity {
-    address public devAddress = 0x6e9407FDDe480aAE460406E7254c8B25F4091dad;
-    
+//SPDX-License-Identifier: UNLICENSED
+address public devAddress = 0x6e9407FDDe480aAE460406E7254c8B25F4091dad;   
 }
